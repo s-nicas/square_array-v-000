16871681dede03b89a21778199e49423de952c67
Sonia  Nicas <sonia.d.nicas@gmail.com>
2018-04-24 03:01:45 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  squared_array=[];
+  array.each do |number|
+    squared_array.push(number*number)
+  end 
+    return squared_array
 end
+
+# solution with .collect 
+
+# def square_array(array)
+#   array.collect{|number| number*number}
+# end 
